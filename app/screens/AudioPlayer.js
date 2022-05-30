48f96b82bd062744e5ea86e965621e2fb9048ec8
Karan Talwar <iamkarantalwar@gmail.com>
2022-05-30 16:32:06 -0700

    Added Slider

--- app/screens/AudioPlayer.js
+++ app/screens/AudioPlayer.js
@@ -1,26 +1,241 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { Audio } from 'expo-av';
+import React, {useRef, useState, useEffect} from 'react';
+import Carousel, {ParallaxImage} from 'react-native-snap-carousel';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  Platform,
+} from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function AudioPlayer() {
-  const [sound, setSound] = React.useState();
+const ENTRIES1 = [
+  {
+    title: 'Beautiful and dramatic Antelope Canyon',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://i.imgur.com/UYiroysl.jpg',
+  },
+  {
+    title: 'Earlier this morning, NYC',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://i.imgur.com/UPrs1EWl.jpg',
+  },
+  {
+    title: 'White Pocket Sunset',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
+    illustration: 'https://i.imgur.com/MABUbpDl.jpg',
+  },
+  {
+    title: 'Acrocorinth, Greece',
+    subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
+    illustration: 'https://i.imgur.com/KZsmUi2l.jpg',
+  },
+  {
+    title: 'The lone tree, majestic landscape of New Zealand',
+    subtitle: 'Lorem ipsum dolor sit amet',
+    illustration: 'https://i.imgur.com/2nCt3Sbl.jpg',
+  },
+];
+const {width: screenWidth} = Dimensions.get('window');
 
-  async function playSound() {
-    const { sound } = await Audio.Sound.createAsync({uri: 'https://dns4.vippendu.com/download/128k-dmmuv/Scapegoat.mp3'});
-    setSound(sound);
-    await sound.playAsync(); }
+const AudioPlayer = (props) => {
+  const [entries, setEntries] = useState([]);
+  const carouselRef = useRef(null);
 
-  React.useEffect(() => {
-    return sound
-      ? () => {
-          console.log('Unloading Sound');
-          sound.unloadAsync(); }
-      : undefined;
-  }, [sound]);
+  const goForward = () => {
+    carouselRef.current.snapToNext();
+  };
 
+  useEffect(() => {
+    setEntries(ENTRIES1);
+  }, []);
+
+  const renderItem = ({item, index}, parallaxProps) => {
+    return (
+      <View style={styles.item}>
+        <Text style={{fontSize: 20, marginLeft: -20, textAlign: 'center', color: 'grey'}} numberOfLines={2}>
+          [stop]
+        </Text>
+        <Text style={{fontSize: 50, marginLeft: -20, textAlign: 'center', color: '#82A4B7'}} numberOfLines={2}>
+        قف 
+        </Text>
+        <ParallaxImage
+          source={{uri: item.illustration}}
+          containerStyle={styles.imageContainer}
+          style={styles.image}
+          parallaxFactor={0.4}
+          {...parallaxProps}
+        />
+      </View>
+    );
+  };
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Button title="Play Sound" onPress={playSound} />
+    <View style={[styles.container, { flexDirection: "column",  backgroundColor: '#fff' }]}>
+        <View
+          style={{
+            flex: 0.2,
+            flexDirection: 'row',
+            backgroundColor: "#82A4B7",
+            borderBottomRightRadius: 45,
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              borderRadius: 100,
+              backgroundColor: "#9D908D",
+              marginTop: 50,
+              marginLeft: 17,
+              width: 35,
+              height: 35,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onPress={() => props.navigation.navigate("Welcome")}
+          >
+            <Text style={{ color: "#D3CFD6", fontWeight: "700" }}>
+              <Text style={styles.back}>
+                <Ionicons name="md-arrow-back" size={24} color="#756765" />
+              </Text>
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.heading}>Words with pictures</Text>
+        </View>
+        <View style={{ flex: 0.7, backgroundColor: "#82A4B7" }}>
+          <View
+            style={{
+              backgroundColor: "#fff",
+              height: "100%",
+              borderTopLeftRadius: 50,
+              paddingTop: 40,
+              flex: 1,
+            }}
+          >
+            <Carousel
+              ref={carouselRef}
+              sliderWidth={screenWidth}
+              itemWidth={screenWidth - 60}
+              data={entries}
+              renderItem={renderItem}
+              hasParallaxImages={true}
+            />
+          </View>
+        </View>
     </View>
   );
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  heading: {
+    marginTop: "11%",
+    marginLeft: 50,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#FFFFF7",
+    marginBottom: 15,
+  },
+  input: {
+    width: "100%",
+    margin: 5,
+    padding: 10,
+    color: "#82A4B7",
+    marginLeft: "auto",
+    marginRight: "auto",
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  view: {
+    backgroundColor: "#82A4B7",
+    height: "100%",
+    width: "100%",
+    borderBottomEndRadius: 50,
+    paddingLeft: 20,
+  },
+  darkContainer: {
+    backgroundColor: "#82A4B7",
+    height: "100%",
+    width: "100%",
+  },
+  bg_white: {
+    backgroundColor: "#FFFFFF",
+    height: "22%",
+    width: "100%",
+    // flex: 0.4,
+  },
+  innerContainer: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 50,
+    height: "100%",
+    width: "100%",
+    flex: 0.4,
+  },
+  back: {
+    marginTop: "10%",
+    fontSize: 15,
+    color: "#FFFFFF",
+  },
+  register: {
+    marginTop: "12%",
+    fontSize: 15,
+    color: "#FFFFFF",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 20,
+    width: "70%",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  color_white: {
+    color: "#FFFFFF",
+  },
+  font_16: {
+    fontSize: 16,
+  },
+  label: {
+    marginTop: 5,
+    fontSize: 15,
+    color: "#82A4B7",
+  },
+  mt_25: {
+    marginTop: 25,
+  },
+  mb_25: {
+    marginBottom: 25,
+  },
+  p_20: {
+    padding: 20,
+  },
+  another_link: {
+    marginTop: 5,
+    fontSize: 15,
+    color: "#82A4B7",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  container: {
+    flex: 1,
+  },
+  item: {
+    width: screenWidth - 60,
+    height: screenWidth - 60,
+  },
+  imageContainer: {
+    flex: 1,
+    marginBottom: Platform.select({ios: 0, android: 1}), // Prevent a random Android rendering issue
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    resizeMode: 'cover',
+  },
+});
+export default AudioPlayer;
