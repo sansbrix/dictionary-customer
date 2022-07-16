a0e0d96ba134261fd5ba49c51fba6aace900eef6
Karan Talwar <iamkarantalwar@gmail.com>
2022-07-17 03:20:39 +0530

    Change In Plan Payment

--- app/screens/PlanPayment.js
+++ app/screens/PlanPayment.js
@@ -6,20 +6,105 @@ import {
   Text,
   SafeAreaView,
   ScrollView,
-  TouchableOpacity
+  TouchableOpacity,
+  Pressable 
 } from "react-native";
 import { confirmPaymentAgent, createPaymentIntent } from "../api";
 import { consoleErrors, showPopUp } from "../helper";
 import { CardField, useStripe } from '@stripe/stripe-react-native';
-import {StripeProvider,  useConfirmPayment } from '@stripe/stripe-react-native';
+import {StripeProvider,  initStripe, useConfirmPayment } from '@stripe/stripe-react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Root } from 'react-native-popup-confirm-toast';
 import Spinner from 'react-native-loading-spinner-overlay';
 
 const cardImage = require('../../assets/cards.png');
 
+
+import { Platform } from 'react-native';
+
+const MethodSelector = ({ onPress, paymentMethod }) => {
+// ...
+return (
+    <View style={{ marginVertical: 48, width: '75%'}}>
+    <Text style={{
+        fontSize: 14,
+        letterSpacing: 1.5,
+        color: 'black',
+        textTransform: 'uppercase'
+    }}>
+        Press Pay to proceed
+    </Text>
+    {/* If there's no paymentMethod selected, show the options */}
+    { !paymentMethod &&
+        <Pressable
+            onPress={onPress}
+            style={{
+                flexDirection: 'row',
+                paddingVertical: 8,
+                alignItems: 'center',
+            }}>
+            {/* {
+                Platform.select({
+                    ios: (<Text style={styles.boldText}>
+                            Apple Pay
+                        </Text>),
+                    android: (<Text style={styles.boldText}>
+                            Google Pay
+                        </Text>)
+                })
+            } */}
+
+            <View style={[styles.selectButton, {marginLeft: 16} ]}>
+                <Text style={[styles.boldText, {color: '#007DFF'}]}>Press Here To Proceed</Text>
+            </View>
+        </Pressable>
+    }
+    {/* If there's a paymentMethod selected, show it */}
+    { !!paymentMethod &&
+        <Pressable
+            onPress={onPress}
+            style={{ 
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingVertical: 8,
+            }}>
+            {paymentMethod.label.toLowerCase().includes('apple') &&
+                <Text style={styles.boldText}>
+                    Apple Pay
+                </Text>
+            }
+            {paymentMethod.label.toLowerCase().includes('google') &&
+                <Text style={styles.boldText}>
+                    Google Pay
+                </Text>
+            }
+            {!paymentMethod.label.toLowerCase().includes('google') &&
+            !paymentMethod.label.toLowerCase().includes('apple') &&
+                <View style={[styles.selectButton, { marginRight: 16 }]}>
+                    <Text style={[styles.boldText, { color: '#007DFF' }]}>
+                        {paymentMethod.label}
+                    </Text>
+                </View>
+            }
+            <Text style={[styles.boldText, { color: '#007DFF', flex: 1 }]}>
+                Change payment method
+            </Text>
+        </Pressable>
+    }
+    </View>
+)}
+
+
 const PlanPayment = (props) => {
   const [loader, setLoader] = React.useState(false);
+  const [ paymentMethod, setPaymentMethod ] = React.useState();
+
+  const {
+      initPaymentSheet,
+      presentPaymentSheet,
+      confirmPaymentSheetPayment,
+  } = useStripe();
 
   const {confirmPayment, loading} = useConfirmPayment();
 
@@ -64,6 +149,78 @@ const PlanPayment = (props) => {
     });
     return response.data.client_secret;
   };
+
+  React.useEffect(() => {
+    (async () => {
+      await initStripe({
+        publishableKey:
+          "pk_test_51JsPtZSFIEz1CbIeM6ziAtoHu8tKLnxPdskn0pu36fWEHJXkGe0J9Wy480tcsaP1YRKut5wymnrz3xPlkNDVbRDO00udjZztjZ",
+        merchantIdentifier: 'yourMerchantIdentifier'
+      });
+
+      // Initialize the PaymentSheet with the paymentIntent data,
+      // we will later present and confirm this
+      await initializePaymentSheet();
+    })();   
+  }, []);
+
+  const initializePaymentSheet = async () => {
+    setLoader(true);
+    const clientSecret = await fetchPaymentIntentClientSecret();
+    const { error, paymentOption  } = await initPaymentSheet({
+        paymentIntentClientSecret: clientSecret,
+        customFlow: true,
+        merchantDisplayName: "Dictionary",
+        style: 'alwaysLight', // If darkMode
+        googlePay: false, // If implementing googlePay
+        applePay: false, // If implementing applePay
+        merchantCountryCode: 'ES', // Countrycode of the merchant
+        // testEnv: __DEV__, // Set this flag if it's a test environment
+    });
+    if (error) {
+        console.log(error)
+    } else {
+        setLoader(false);
+        // Upon initializing if there's a paymentOption
+        // of choice it will be filled by default
+        setPaymentMethod(paymentOption);
+    }
+  };
+
+  const handleSelectMethod = async () => {
+    const { error, paymentOption } = await presentPaymentSheet({
+        confirmPayment: false,
+    });
+    if (error) {
+        alert(`Error code: ${error.code}`, error.message);
+    }
+    setPaymentMethod(paymentOption);
+  }
+
+  const handleBuyPress = async () => {
+    setLoader(true);
+    if (paymentMethod) {
+        const response = await confirmPaymentSheetPayment();
+
+        if (response.error) {
+            showPopUp(response.error.message, true);
+            console.error(response.error.message);
+        } else {
+          const paymentConfirmDB = await confirmPaymentAgent(props.route.params.plan);
+          if(paymentConfirmDB) {
+            showPopUp("Subscription has been activated");
+              setTimeout(() => {
+            props.navigation.navigate('MainMenu')
+          }, 3000);
+          } else {
+            showPopUp("There was internal server error.Please contact Admin, your payment has been proceed.", true);
+          }
+          
+        }
+
+        setLoader(false);
+    }
+  }
 
   return (
     <SafeAreaView style={[styles.container, { flexDirection: "column" }]}>
@@ -112,20 +269,24 @@ const PlanPayment = (props) => {
                     merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" 
                     // required for Apple Pay
                   >
-                  
-                    <CardField
+                    <MethodSelector onPress={handleSelectMethod} paymentMethod={paymentMethod} />
+                    {/* <CardField
                       postalCodeEnabled={true}
                       placeholder={{
                         number: '4242 4242 4242 4242',
                       }}
-                      cardStyle={{
-                        backgroundColor: '#FFFFFF',
-                        textColor: '#000000',
-                      }}
                       style={{
-                        width: '100%',
-                        height: 50,
-                        marginVertical: 30,
+                        height: 200,
+                        // justifyContent: "center",
+                        // alignItems: "center",
+                        // textAlign: "center",
+                        display: "flex",
+                        flexDirection: "row",
+                      }}
+                      cardStyle={{
+                        backgroundColor: "#efefefef",
+                        textAlign: "center",
+                        textColor: "pink",
                       }}
                       onCardChange={(cardDetails) => {
                         console.log('cardDetails', cardDetails);
@@ -133,8 +294,8 @@ const PlanPayment = (props) => {
                       onFocus={(focusedField) => {
                         console.log('focusField', focusedField);
                       }}
-                    />
-                    <Button onPress={handlePayPress} title="Pay" disabled={loading} />
+                    /> */}
+                    {paymentMethod && <Button onPress={() => handleBuyPress()} title="Pay" disabled={loading} />}
                   
                   </StripeProvider>
                 </View>
